1827293435ffde417853250057bf397079b7d4e6
Sathsara Maduranga <sathsara.ican@gmail.com>
2023-10-02 08:35:26 +0530

    tab bar added

---
+++ ColomboCC/Tab Bar/TabBarVC.swift
@@ -0,0 +1,20 @@
+//
+//  TabBarVC.swift
+//  ColomboCC
+//
+//  Created by Sathsara Dharmarathna on 2023-10-02.
+//
+
+import UIKit
+
+class TabBarVC: UITabBar {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
